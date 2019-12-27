25ed0831a6255f72329f536f79a233df5bf6ab1a
shutoskate <wizwizwayne@gmail.com>
2019-12-27 16:57:39 +0900

    associate
    userとの紐付け
    誰の投稿かがわかる

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_26_075739) do
+ActiveRecord::Schema.define(version: 2019_12_26_101200) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "kill", null: false
     t.text "title", null: false
     t.string "author"
     t.string "image"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -29,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_12_26_075739) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
