5584a9df1bb6cb256151b2427c1edd218bfdaba7
shutoskate <wizwizwayne@gmail.com>
2020-02-07 00:34:48 +0900

    s3保存のため

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   validates :title, :kill, :author, :image, presence: true
   mount_uploader :image, ImageUploader#画像アップロードgem carrierwave読み込み
+  has_one_attached :image
 
   belongs_to :user
   has_many :favorites
